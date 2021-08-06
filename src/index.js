edfcf6a0a902b15bad11763eee43ad4db1206a70
Antonio32A <~@antonio32a.com>
2021-08-06 20:56:25 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-const handleRequest = () =>
-    new Response(JSON.stringify({ foo: "bar" }), { headers: { "Content-Type": "application/json" } });
+import lilyweight from "lilyweight";
+
+const handleRequest = async request => {
+    const url = new URL(request.url);
+    const key = url.searchParams.get("key");
+    const player = url.pathname.substr(1);
+    const returnUsername = url.searchParams.get("returnUsername") === "true" ?? false;
+
+    if (!key) return respond({ success: false, error: "No Hypixel API key provided." });
+    if (!player) return respond({ success: false, error: "No player provided." });
+
+    const lily = lilyweight(key);
+    try {
+        return respond({ success: true, data: await lily.getWeight(player, returnUsername) });
+    } catch {
+        return respond({ success: false, error: "Invalid API key or player provided." });
+    }
+};
+
+const respond = data => new Response(JSON.stringify(data), { headers: { "Content-Type": "application/json" } });
 
 addEventListener("fetch", event => event.respondWith(handleRequest(event.request)));
